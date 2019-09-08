08f630f46a6266803be830dbb4ee84245b823c0e
Usuario de Salon <aulas@academico.icesi.edu.co>
2019-09-08 12:51:22 -0500

    Added the implementation of heap

---
+++ src/Collections/MaxHeap.java
@@ -0,0 +1,58 @@
+package Collections;
+
+public class MaxHeap <T extends Comparable<T>>{
+	//Constant
+		public final static int DEFAULT_CAPACITY = 100;
+	
+	//Attributes
+		private T[] heap;
+		private int size;
+	
+	//Methods
+		
+		//Builder
+			@SuppressWarnings("unchecked")
+			public MaxHeap() {
+				heap = (T[]) new Object[DEFAULT_CAPACITY]; 
+			}
+			
+			@SuppressWarnings("unchecked")
+			public MaxHeap(int capacity) {
+				heap = (T[]) new Object[capacity];
+			}
+			
+			public MaxHeap(T[] array) {
+				heap = array;
+				size = array.length;
+			}
+		
+		//Getters
+			public int getSize() {
+				return size;
+			}
+		
+		//Operational
+			public int heapLenght() {
+				return heap.length;
+			}
+			
+			public void heapify(int i) {
+				
+			}
+			
+			public int left(int i) {	
+				return 2*i;
+			}
+			
+			public int right(int i) {
+				return 2*i + 1;
+			}
+			
+			public int parent(int i) {
+				return i/2;
+			}
+	
+	
+	
+	
+}
